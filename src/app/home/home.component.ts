96e12cbc2a55955943afa61611d84c7a7bcb6d62
Ronan Connolly <Ronan@RonanConnolly.dev>
2024-04-29 19:50:57 +0200

    Add in mat toolbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { JokesComponent } from '../jokes/jokes.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'cnjg-home',
   standalone: true,
-  imports: [JokesComponent],
+  imports: [JokesComponent, MatIconModule, MatButtonModule, MatToolbarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   // changeDetection: ChangeDetectionStrategy.OnPush,
